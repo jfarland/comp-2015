c4c3c087d094c4d8e3cee324ff19e4ce5fcf30bb
Jon Farland <jon.farland@dnvgl.com>
2015-09-30 19:27:49 -0400

    finished the processing code for the PJM load data for dominion and provided simple plots

--- code/00-manage-data.R
+++ code/00-manage-data.R
@@ -11,7 +11,13 @@
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
 
 library("ggplot2")
+library("lattice")
 library("dplyr")
+library("tidyr")
+library("gdata")
+library("reshape2")
+
+
 
 
 #-----------------------------------------------------------------------------#
@@ -36,6 +42,9 @@ data<-read.csv()
 #R data set
 data<-readRDS()
 
+#potential to run Python program in order to download excel files again here
+#system('python /home/rstudio/projects/comp-2015/data/rawdat/00-pull-historical-load-data.py')
+
 
 #-----------------------------------------------------------------------------#
 #
@@ -43,35 +52,52 @@ data<-readRDS()
 #
 #-----------------------------------------------------------------------------#
 
-library(gdata)
-library(dplyr)
-
 #set the raw data as the current directory
 setwd("/home/jonfar/Projects/comp-2015/data/rawdat")
 
 #Read in only the dominion tab of the excel spreadsheets
-load11 <- read.xls("load11.xls", sheet=22)
-load12 <- read.xls("load12.xls", sheet=22)
-load13 <- read.xls("load13.xls", sheet=22)
-load14 <- read.xls("load14.xls", sheet=22)
-load15 <- read.xls("load15.xls", sheet=22)
+load11 <- read.xls("load11.xls", sheet=22) %>%
+  select(DATE:HE24)
+load12 <- read.xls("load12.xls", sheet=22) %>%
+  select(DATE:HE24)
+load13 <- read.xls("load13.xls", sheet=22) %>%
+  select(DATE:HE24)
+load14 <- read.xls("load14.xls", sheet=22) %>%
+  select(DATE:HE24)
+load15 <- read.xls("load15.xls", sheet=22) %>%
+  select(DATE:HE24)
 
-load.data=rbind(load11, load12, load13, load14)#, load15)
+load.data=rbind(load11, load12, load13, load14, load15)
 
-#for (i in 11:15){
-#  load.data[[i-10]] <- read.xls(paste("load", i, ".xls", sep=""), sheet = 22)
-#}
+#go from wide to long
+load.long <- melt(load.data, id=c("DATE", "COMP")) %>%
+  rename(hour = variable, load = value) %>%
+  mutate(tindx = mdy_h(paste(DATE, substr(hour, 3, 4)))-duration(1,"hours"),
+         hindx = hour(tindx),
+         dindx = as.Date(tindx),
+         mindx = month(tindx)) %>%
+  select(tindx, hindx, dindx, mindx, load)
 
-#do.call(rbind, load.data)
-
-#load.data_ul <- unlist(load.data)
-
-
+#shifted to hour beginning rather than hour ending
 
 #quick checks
-#check1<-stopifnot(load11$comp == "DOM")
-summary(load11)
+summary(load.long)
 
+#-----------------------------------------------------------------------------#
+#
+# Graphics
+#
+#-----------------------------------------------------------------------------#
+
+# load over time
+plot1 <- plot(load.long$load ~ load.long$tindx)
+plot2 <- plot(load.long$load ~ load.long$hindx)
+plot3 <- plot(load.long$load ~ load.long$dindx)
+
+#histograms and conditional histograms
+histogram(~load | mindx, data = load.long, xlab="Load (MW)", ylab ="Density", col=c("red"))
+histogram(~load | hindx, data = load.long, xlab="Load (MW)", ylab ="Density", col=c("red"))
+histogram(~load , data = load.long, xlab="Load (MW)", ylab ="Density", col=c("red"))
 
 
 
@@ -80,7 +106,6 @@ summary(load11)
 # Outputs
 #
 #-----------------------------------------------------------------------------#
-
 
 write.csv()
 writeRDS()
