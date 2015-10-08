34eeb3bf49cf8b896dcb4a255d34f9e1d2a7fa51
Jon Farland <jon.farland@dnvgl.com>
2015-10-07 20:40:16 -0400

    completed funcationaly of downloading the 10 day forecast for temperature at Richmond airport

--- data/rawdat/00-pull-forecasted-weather.py
+++ data/rawdat/00-pull-forecasted-weather.py
@@ -1,5 +1,4 @@
-
-import sys, csv, json, urllib2, requests
+import sys, csv, json, urllib2, requests, datetime
 
 f = urllib2.urlopen('http://api.wunderground.com/api/c9be88eb723364fc/hourly10day/q/VA/Richmond.json')
 
@@ -12,11 +11,35 @@ parsed_json.keys()
 #check to make sure it's a dictionary
 type(parsed_json[u'hourly_forecast'])
 
-#look at the top obs
-parsed_json[u'hourly_forecast'][:20]
-
 #we could output the json file as text now
-with open('weather-forecasts.txt', 'w') as outfile:
+with open('json-file.txt', 'w') as outfile:
   json.dump(parsed_json, outfile)
+
+#look at just the forecasts
+forecasts = parsed_json[u'hourly_forecast']
+
+#we could pull apart each datetime element
+temp  = [int(x[u'temp'][u'english']) for x in forecasts]
+hour  = [int(x[u'FCTTIME'][u'hour']) for x in forecasts]
+year  = [int(x[u'FCTTIME'][u'year']) for x in forecasts]
+month = [int(x[u'FCTTIME'][u'mon'])  for x in forecasts]
+day   = [int(x[u'FCTTIME'][u'mday']) for x in forecasts]
+
+#make lists of dates and time
+date =[datetime.date(2015,10,d) for d in days]
+time =[datetime.time(h,0,0) for h in hour]
+
+#attempt to combine the data
+datetime = [datetime.datetime.combine(d,t) for d in date]
+
+#do it all concisely and save as tuples.
+data = [(int(x[u'temp'][u'english']),int(x[u'FCTTIME'][u'hour']), int(x[u'FCTTIME'][u'year']),int(x[u'FCTTIME'][u'mon']),int(x[u'FCTTIME'][u'mday']))for x in forecasts]
+
+with open('temp-forecasts.csv','w') as out:
+  csv_out=csv.writer(out)
+  csv_out.writerow(['temp','hindx','year','mindx','dindx'])
+  for row in data:
+    csv_out.writerow(row)
+
 
 
